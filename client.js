74ebcda0d2a136ad0ca6fab7f15426f7382eb5f3
Lara Lee <root@laralee.me>
2021-04-06 17:41:41 -0700

    Tweak example frontend
    Not meant to be final, frontend will be separate react app

--- client.js
+++ client.js
@@ -1,58 +1,56 @@
-const productsToCheck = {
-  items: [
-    {
-      name: 'PS5',
-      selector: '#add-to-cart-button',
-      url: 'https://www.amazon.com/PlayStation-5-Console/dp/B08FC5L3RG'
-    },
-    {
-      name: '3080 Asus TUF OC',
-      selector: '#add-to-cart-button',
-      url: 'https://www.amazon.com/ASUS-GeForce-Graphics-DisplayPort-Bearings/dp/B08HH5WF97'
-    },
-    {
-      name: '3080 Asus TUF',
-      selector: '#add-to-cart-button',
-      url: 'https://www.amazon.com/ASUS-Graphics-DisplayPort-Military-Grade-Certification/dp/B08HHDP9DW'
-    },
-    {
-      name: '3080 EVGA FTW3',
-      selector: '#add-to-cart-button',
-      url: 'https://www.amazon.com/EVGA-10G-P5-3895-KR-GeForce-Technology-Backplate/dp/B08HR3DPGW'
-    },
-    {
-      name: '3080 EVGA FTW3 ULTRA',
-      selector: '#add-to-cart-button',
-      url: 'https://www.amazon.com/EVGA-10G-P5-3897-KR-GeForce-Technology-Backplate/dp/B08HR3Y5GQG'
-    },
-    {
-      name: '3080 EVGA XC3',
-      selector: '#add-to-cart-button',
-      url: 'https://www.amazon.com/EVGA-10G-P5-3883-KR-GeForce-Cooling-Backplate/dp/B08HR4RJ3Q'
-    },
-    {
-      name: '3080 EVGA XC3 BLACK',
-      selector: '#add-to-cart-button',
-      url: 'https://www.amazon.com/EVGA-10G-P5-3881-KR-GeForce-GAMING-Cooling/dp/B08HR6FMF3'
-    },
-    {
-      name: '3080 EVGA XC3 ULTRA',
-      selector: '#add-to-cart-button',
-      url: 'https://www.amazon.com/EVGA-10G-P5-3885-KR-GeForce-Cooling-Backplate/dp/B08HR55YB5'
-    },
-    {
-      name: '3070 Asus OC',
-      selector: '#add-to-cart-button',
-      url: 'https://www.amazon.ca/Graphics-DisplayPort-Axial-tech-Protective-Backplate/dp/B08L8LG4M3'
-    },
-    {
-      name: 'Nintendo Switch Pro Controller',
-      note: 'control item, should be in stock',
-      selector: '#add-to-cart-button',
-      url: 'https://www.amazon.com/Nintendo-Switch-Pro-Controller/dp/B01NAWKYZ0'
-    }
-  ]
-};
+const productsToCheck = [
+  {
+    name: 'PS5',
+    selector: '#add-to-cart-button',
+    url: 'https://www.amazon.com/PlayStation-5-Console/dp/B08FC5L3RG'
+  },
+  {
+    name: '3080 Asus TUF OC',
+    selector: '#add-to-cart-button',
+    url: 'https://www.amazon.com/ASUS-GeForce-Graphics-DisplayPort-Bearings/dp/B08HH5WF97'
+  },
+  {
+    name: '3080 Asus TUF',
+    selector: '#add-to-cart-button',
+    url: 'https://www.amazon.com/ASUS-Graphics-DisplayPort-Military-Grade-Certification/dp/B08HHDP9DW'
+  },
+  {
+    name: '3080 EVGA FTW3',
+    selector: '#add-to-cart-button',
+    url: 'https://www.amazon.com/EVGA-10G-P5-3895-KR-GeForce-Technology-Backplate/dp/B08HR3DPGW'
+  },
+  {
+    name: '3080 EVGA FTW3 ULTRA',
+    selector: '#add-to-cart-button',
+    url: 'https://www.amazon.com/EVGA-10G-P5-3897-KR-GeForce-Technology-Backplate/dp/B08HR3Y5GQG'
+  },
+  {
+    name: '3080 EVGA XC3',
+    selector: '#add-to-cart-button',
+    url: 'https://www.amazon.com/EVGA-10G-P5-3883-KR-GeForce-Cooling-Backplate/dp/B08HR4RJ3Q'
+  },
+  {
+    name: '3080 EVGA XC3 BLACK',
+    selector: '#add-to-cart-button',
+    url: 'https://www.amazon.com/EVGA-10G-P5-3881-KR-GeForce-GAMING-Cooling/dp/B08HR6FMF3'
+  },
+  {
+    name: '3080 EVGA XC3 ULTRA',
+    selector: '#add-to-cart-button',
+    url: 'https://www.amazon.com/EVGA-10G-P5-3885-KR-GeForce-Cooling-Backplate/dp/B08HR55YB5'
+  },
+  {
+    name: '3070 Asus OC',
+    selector: '#add-to-cart-button',
+    url: 'https://www.amazon.ca/Graphics-DisplayPort-Axial-tech-Protective-Backplate/dp/B08L8LG4M3'
+  },
+  {
+    name: 'Nintendo Switch Pro Controller',
+    note: 'control item, should be in stock',
+    selector: '#add-to-cart-button',
+    url: 'https://www.amazon.com/Nintendo-Switch-Pro-Controller/dp/B01NAWKYZ0'
+  }
+];
 
 // Store for all of the jobs in progress
 let jobs = {};
@@ -64,15 +62,10 @@ async function addJob (data) {
     headers: {
       'Content-Type': 'application/json'
     },
-    // body: JSON.stringify(data)
-    body: JSON.stringify({
-      name: '3070 Asus OC',
-      selector: '#add-to-cart-button',
-      url: 'https://www.amazon.ca/Graphics-DisplayPort-Axial-tech-Protective-Backplate/dp/B08L8LG4M3'
-    })
+    body: JSON.stringify(productsToCheck[Math.floor(Math.random() * productsToCheck.length)])
   });
-  const job = await res.json();
-  jobs[job.id] = { id: job.id, state: 'queued' };
+  const resJson = await res.json();
+  jobs[resJson.id] = { id: resJson.id + ' | ' + resJson.job.data.name, state: 'queued' };
   render();
 }
 
@@ -80,9 +73,11 @@ async function addJob (data) {
 async function updateJobs () {
   for (const id of Object.keys(jobs)) {
     const res = await fetch(`/job/${id}`);
-    const result = await res.json();
+    const resJson = await res.json();
     if (jobs[id]) {
-      jobs[id] = result;
+      jobs[id].state = resJson.state;
+      jobs[id].progress = resJson.progress;
+      jobs[id].result = resJson.result;
     }
     render();
   }
@@ -120,10 +115,13 @@ function renderJob (job) {
     progress = 100;
   }
 
+  console.log('job ', job);
+
   return document.querySelector('#job-template')
     .innerHTML
     .replace('{{id}}', job.id)
     .replace('{{state}}', job.state)
+    .replace('{{result}}', job.result ? job.result.inStock : 'waiting')
     .replace('{{color}}', color)
     .replace('{{progress}}', progress);
 }
